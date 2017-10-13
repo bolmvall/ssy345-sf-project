b7ed3dd6d216f0b5cf9f9d94ecc94e3eee75c5aa
Gunnar Bolmvall <Gunnar.Bolmvall@gmail.com>
2017-10-13 11:15:56 +0200

    Updated source code

--- src/mu_normalizeQ.m
+++ src/mu_normalizeQ.m
@@ -2,5 +2,7 @@ function [x, P] = mu_normalizeQ(x, P)
 % MU_NORMALIZEQ  Normalize the quaternion
 
   x(1:4)  = x(1:4) / norm(x(1:4));
-  x = x*sign(x(1));
+  if x(1) < 0,
+    x(1:4) = -x(1:4);
+  end
 end
