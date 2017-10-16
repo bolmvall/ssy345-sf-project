c97d9e6d0b075f7b927d2ec977589e6cab95fdea
Gunnar Bolmvall <Gunnar.Bolmvall@gmail.com>
2017-10-16 19:30:12 +0200

    Adding function to plot data vs time, one figure for each sensor

--- main.m
+++ main.m
@@ -73,7 +73,7 @@ for i=1:3
         end
     end
     % Save image
-    saveas(gca,['./fig/hist-' sensor{i}],'epsc')
+    %saveas(gca,['./fig/hist-' sensor{i}],'epsc')
 end
 
 %% Generate time plots of data
@@ -99,7 +99,66 @@ for i=1:3
 end
 % Save figure
 saveas(gca,['./fig/time-agm'],'epsc')
+%% Generate One time plot for each sensor
+% Define limits
+ylimits = [0.05 0.01 3];
+% For each sensor
+for i=1:3
+    figure(i)
+    % All axes
+    for j=1:3
+        subplot(3,1,j)
+        plot(meas.t,to_plot{i}(j,:),'Color',cm(j,:))
+        % Add legend
+        legend(['$' ax{j} '$']),ylim([-ylimits(i) ylimits(i)])
+        % Add title on first plot
+        if j==1
+            title(['$' sensor{i} '\ Data$']),box on
+        end
+        % Add ylabel on mid plot
+        if j==2
+            ylabel('$Sensor \ value$','FontSize',15)
+        end
+        % Add xlabel on last subplot
+        if j==3
+            xlabel('$t$','FontSize',15)
+        end
+    end
+    % Save plot
+    saveas(gca,['./fig/time-' sensor{i}],'epsc')
+end
+% Save figure
+saveas(gca,['./fig/time-agm'],'epsc')
 
+%% Collect data using modified filter
+[xhat, meas] = filterTemplate2();
+
+%% Plot euler angles
+euler_filter = q2euler(xhat.x);
+euler_phone = q2euler(meas.orient);
+
+for i=1:size(euler_filter,1)
+    subplot(3,1,i),box on
+    plot(xhat.t,(euler_filter(i,:)),'-','Color',cm(i,:))
+    hold on
+    plot(xhat.t,(euler_phone(i,:)),'k--')
+    legend(['$' ax{i} '_{f}$'],['$' ax{i} '_{g}$'],'Location','NorthEast')
+    
+    % Add title on top subplokt
+    if i==1
+        title(['$Angle \ comparison \ - \ Filter \ vs \ Google$'])
+    end
+    % Add ylabel on mid subplot
+    if i==2
+        ylabel('$rad$')
+    end
+    % Add xlabel on last subplot
+    if i==3
+        xlabel('$t$')
+    end
+    hold off
+end
+saveas(gca,'./fig/comparison','epsc')
 %% Collect data using modified filter
 [xhat, meas] = filterTemplate2();
 
